171568a401fd1ab2534824ba0ba89f0c35ec0b72
HaoD2 <73094389+HaoD2@users.noreply.github.com>
2024-05-22 21:49:54 +0700

    terbaru untuk graph

--- Admin/admin_flutter/lib/route/laporan/laporan_insight/LI_Dekstop.dart
+++ Admin/admin_flutter/lib/route/laporan/laporan_insight/LI_Dekstop.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:d_chart/d_chart.dart';
 import 'package:intl/intl.dart';
 import 'package:admin_flutter/navigation/navigation_header/nav_responsive.dart';
 import 'package:admin_flutter/navigation/navigation_side/nav_responsive.dart';
@@ -16,100 +17,67 @@ class LaporanInsightDekstop extends StatefulWidget {
 
 class _LaporanInsightDekstopState extends State<LaporanInsightDekstop> {
   FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  double temp = 0.0;
-  List<FlSpot> _spots = [];
-  List<FlSpot> _data_Money = [];
-  Map<int, Widget> _titleCache = {};
-  List<String> _jobTypes = [];
-  double maxY = 0;
-  String errorMessage = '';
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
   void initState() {
     super.initState();
-    _fetchData();
-    _fetchDataMoney();
+    // _fetchDataPekerjaan();
+    // _fetchDataMoney();
   }
 
-  Future<void> _fetchData() async {
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    QuerySnapshot querySnapshot =
-        await firestore.collection('request_handyman').get();
-    Map<String, int> jobTypeCounts = {};
+  // Future<Map<String, int>> _fetchDataPekerjaan() async {
+  //   try {
+  //     QuerySnapshot snapshot =
+  //         await FirebaseFirestore.instance.collection('request_handyman').get();
+  //     Map<String, int> pekerjaanCount = {};
+  //     Map<String, Set<String>> monthSet = {};
+  //     List<String> months = [
+  //       'January',
+  //       'February',
+  //       'March',
+  //       'April',
+  //       'May',
+  //       'June',
+  //       'July',
+  //       'August',
+  //       'September',
+  //       'October',
+  //       'November',
+  //       'December'
+  //     ];
 
-    for (var doc in querySnapshot.docs) {
-      String jobType = doc['tipe_pekerjaan'];
+  //     for (var doc in snapshot.docs) {
+  //       String tipePekerjaan = doc['tipe_pekerjaan'];
+  //       Timestamp createdDate = doc['created_date'];
+  //       DateTime date = createdDate.toDate();
+  //       String month = months[date.month - 1]; // Mengambil nama bulan
 
-      if (jobTypeCounts.containsKey(jobType)) {
-        jobTypeCounts[jobType] = jobTypeCounts[jobType]! + 1;
-      } else {
-        jobTypeCounts[jobType] = 1;
-      }
-    }
+  //       // Menghitung jumlah pesanan berdasarkan tipe pekerjaan
+  //       if (pekerjaanCount.containsKey(tipePekerjaan)) {
+  //         pekerjaanCount[tipePekerjaan] = pekerjaanCount[tipePekerjaan]! + 1;
+  //       } else {
+  //         pekerjaanCount[tipePekerjaan] = 1;
+  //       }
 
-    // Convert keys to a set to remove duplicates, then back to a list
-    List<String> jobTypes = jobTypeCounts.keys.toSet().toList();
+  //       // Menyimpan bulan tanpa duplikasi untuk tipe pekerjaan tersebut
+  //       if (!monthSet.containsKey(tipePekerjaan)) {
+  //         monthSet[tipePekerjaan] = <String>{};
+  //       }
+  //       monthSet[tipePekerjaan]!.add(month);
+  //     }
 
-    List<FlSpot> spots = jobTypeCounts.entries.map((entry) {
-      int index = jobTypes.indexOf(entry.key);
-// Debugging print
-      return FlSpot(index.toDouble(), entry.value.toDouble());
-    }).toList();
+  //     // Mengkonversi monthSet ke pekerjaanCount untuk bulan
+  //     for (var key in monthSet.keys) {
+  //       pekerjaanCount[key] = monthSet[key]!.length;
+  //     }
 
-    setState(() {
-      _spots = spots;
-      _jobTypes = jobTypes;
-    });
-  }
-
-  Future<void> _fetchDataMoney() async {
-    try {
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-          .collection('request_handyman')
-          .where('status',
-              whereIn: ['pending', 'on-progress', 'success']).get();
-
-      Map<String, double> monthlyEarnings = {};
-
-      for (var doc in querySnapshot.docs) {
-        DateTime date = doc['created_date'].toDate();
-        String monthYear = DateFormat('yyyy-MM').format(date);
-        double price;
-        try {
-          price = double.parse(doc['price'].toString());
-        } catch (e) {
-          print('Failed to parse price for document ${doc.id}: $e');
-          continue; // Skip this document if price is not valid
-        }
-
-        if (monthlyEarnings.containsKey(monthYear)) {
-          monthlyEarnings[monthYear] = monthlyEarnings[monthYear]! + price;
-        } else {
-          monthlyEarnings[monthYear] = price;
-        }
-      }
-
-      List<FlSpot> spots_money = [];
-      List<String> sortedKeys = monthlyEarnings.keys.toList()..sort();
-      for (int i = 0; i < sortedKeys.length; i++) {
-        double totalPrice = monthlyEarnings[sortedKeys[i]]!;
-        spots_money.add(FlSpot(i.toDouble(), totalPrice));
-        if (totalPrice > maxY) {
-          maxY = totalPrice;
-        }
-      }
-
-      setState(() {
-        _data_Money = spots_money;
-      });
-    } catch (e) {
-      setState(() {
-        errorMessage = 'Failed to fetch data: $e';
-      });
-    }
-  }
-
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  //     return pekerjaanCount;
+  //   } catch (e) {
+  //     print("Error fetching data: $e");
+  //     throw e;
+  //   }
+  // }
 
   Future<void> sendDataToAnalytics() async {
     // Mendapatkan data dari Firestore
@@ -158,194 +126,45 @@ class _LaporanInsightDekstopState extends State<LaporanInsightDekstop> {
             child: Row(
               children: [
                 Container(child: NavigationSideResponsive()),
-                Container(
-                  child: Column(children: [
-                    Expanded(
-                      child: Center(
-                        child: Column(children: [
-                          SizedBox(
-                            height: 50,
-                          ),
-                          Container(
-                              width: 500,
-                              height: 300,
-                              child: _spots.isEmpty
-                                  ? CircularProgressIndicator()
-                                  : LineChart(LineChartData(
-                                      minY: 0,
-                                      maxY: temp + 20,
-                                      gridData: FlGridData(
-                                        show: true,
-                                      ),
-                                      titlesData: FlTitlesData(
-                                        show: true,
-                                        leftTitles: AxisTitles(
-                                          axisNameSize: 15,
-                                          axisNameWidget: Container(
-                                              child: Text(
-                                                  'Banyak Pesanan dalam Layanan')),
-                                          sideTitles: SideTitles(
-                                            showTitles: true,
-                                            getTitlesWidget:
-                                                (double value, TitleMeta meta) {
-                                              return SideTitleWidget(
-                                                axisSide: meta.axisSide,
-                                                child: Text(
-                                                  value.toString(),
-                                                  style: TextStyle(fontSize: 7),
-                                                ),
-                                              );
-                                            },
-                                          ),
-                                        ),
-                                        bottomTitles: AxisTitles(
-                                          sideTitles: SideTitles(
-                                            showTitles: true,
-                                            getTitlesWidget:
-                                                (double value, TitleMeta meta) {
-                                              int index = value.toInt();
-                                              if (!_titleCache
-                                                  .containsKey(index)) {}
+                // Container(
+                //   width: 250,
+                //   height: 500,
+                //   child: FutureBuilder<Map<String, int>>(
+                //     future: _fetchDataPekerjaan(),
+                //     builder: (context, snapshot) {
+                //       if (snapshot.connectionState == ConnectionState.waiting) {
+                //         return Center(child: CircularProgressIndicator());
+                //       } else if (snapshot.hasError) {
+                //         return Center(child: Text('Error: ${snapshot.error}'));
+                //       } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                //         return Center(child: Text('No data available'));
+                //       } else {
+                //         Map<String, int> pekerjaanData = snapshot.data!;
+                //         List<String> pekerjaanKeys =
+                //             pekerjaanData.keys.toList();
+                //         List<int> pekerjaanValues =
+                //             pekerjaanData.values.toList();
 
-                                              // Caching title widget
-                                              if (index >= 0 &&
-                                                  index < _jobTypes.length) {
-                                                if (!_titleCache
-                                                    .containsKey(index)) {
-                                                  _titleCache[index] =
-                                                      SideTitleWidget(
-                                                    axisSide: meta.axisSide,
-                                                    child: Text(
-                                                      _jobTypes[index],
-                                                      style: TextStyle(
-                                                          fontSize: 7),
-                                                    ),
-                                                  );
-                                                }
-                                                return _titleCache[index]!;
-                                              } else {
-                                                if (!_titleCache
-                                                    .containsKey(index)) {
-                                                  _titleCache[index] =
-                                                      SideTitleWidget(
-                                                    axisSide: meta.axisSide,
-                                                    child: Text(''),
-                                                  );
-// Debugging print
-                                                }
-                                                return _titleCache[index]!;
-                                              }
-                                            },
-                                          ),
-                                        ),
-                                        topTitles: AxisTitles(
-                                          sideTitles:
-                                              SideTitles(showTitles: false),
-                                        ),
-                                        rightTitles: AxisTitles(
-                                          sideTitles:
-                                              SideTitles(showTitles: false),
-                                        ),
-                                      ),
-                                      borderData: FlBorderData(show: true),
-                                      lineBarsData: [
-                                        LineChartBarData(
-                                          spots: _spots,
-                                          isCurved: true,
-                                          color: Colors.blue,
-                                          barWidth: 4,
-                                          belowBarData: BarAreaData(
-                                            show: true,
-                                            color: Colors.blue.withOpacity(0.3),
-                                          ),
-                                        ),
-                                      ],
-                                    ))),
-                        ]),
-                      ),
-                    )
-                  ]),
-                ),
-                Container(
-                  width: 500,
-                  height: 300,
-                  child: _data_Money.isEmpty
-                      ? CircularProgressIndicator()
-                      : LineChart(
-                          LineChartData(
-                            minY: 0,
-                            maxY: maxY + 20,
-                            gridData: FlGridData(
-                              show: true,
-                            ),
-                            titlesData: FlTitlesData(
-                              show: true,
-                              leftTitles: AxisTitles(
-                                axisNameSize: 15,
-                                axisNameWidget:
-                                    Container(child: Text('Total Uang (IDR)')),
-                                sideTitles: SideTitles(
-                                  showTitles: true,
-                                  getTitlesWidget:
-                                      (double value, TitleMeta meta) {
-                                    return SideTitleWidget(
-                                      axisSide: meta.axisSide,
-                                      child: Text(
-                                        value.toString(),
-                                        style: TextStyle(fontSize: 7),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                              bottomTitles: AxisTitles(
-                                sideTitles: SideTitles(
-                                  showTitles: true,
-                                  getTitlesWidget:
-                                      (double value, TitleMeta meta) {
-                                    int index = value.toInt();
-                                    if (index >= 0 &&
-                                        index < _data_Money.length) {
-                                      return SideTitleWidget(
-                                        axisSide: meta.axisSide,
-                                        child: Text(
-                                          DateFormat('MMM').format(
-                                              DateTime(2024, index + 1)),
-                                          style: TextStyle(fontSize: 7),
-                                        ),
-                                      );
-                                    } else {
-                                      return SideTitleWidget(
-                                        axisSide: meta.axisSide,
-                                        child: Text(''),
-                                      );
-                                    }
-                                  },
-                                ),
-                              ),
-                              topTitles: AxisTitles(
-                                sideTitles: SideTitles(showTitles: false),
-                              ),
-                              rightTitles: AxisTitles(
-                                sideTitles: SideTitles(showTitles: false),
-                              ),
-                            ),
-                            borderData: FlBorderData(show: true),
-                            lineBarsData: [
-                              LineChartBarData(
-                                spots: _data_Money,
-                                isCurved: true,
-                                color: Colors.blue,
-                                barWidth: 4,
-                                belowBarData: BarAreaData(
-                                  show: true,
-                                  color: Colors.blue.withOpacity(0.3),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                ),
+                //         // Membuat chart berdasarkan data yang diambil
+                //         return AspectRatio(
+                //           aspectRatio: 16 / 9,
+                //           child: DChartBarO(
+                //             groupList: pekerjaanKeys.map((key) {
+                //               return OrdinalGroup(
+                //                 id: key,
+                //                 data: [
+                //                   OrdinalData(
+                //                       domain: key,
+                //                       measure: pekerjaanData[key] ?? 0),
+                //                 ],
+                //               );
+                //             }).toList(),
+                //           ),
+                //         );
+                //       }
+                //     },
+                //   ),
+                // ),
               ],
             ),
           ),
